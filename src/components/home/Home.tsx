fceb20a4dfaf2dba87702a3ab618606bd6d80cdb
Shoaib Chikate <shoaibista@gmail.com>
2023-09-18 23:20:01 +0530

    new changes

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,17 +3,39 @@ import './Home.css';
 import type { DatePickerProps } from 'antd';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import dayjs from 'dayjs';
-import { PlusOutlined } from '@ant-design/icons';
+import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 
 export const Home: React.FC = () => {
 
+   console.log('test');
    const calendarTypes = [
       { label: 'Test Type', value: 'TestValue' }
    ];
 
+   
+
+
+   const holidayList = [{
+      id: 1,
+      holidayName: 'Christmas',
+      holidayDate: '25th Dec, 2023'   
+   }, {
+      id: 2,
+      holidayName: 'New Year Eve',
+      holidayDate: '30th Dec, 2023'
+   }, {
+      id: 3,
+      holidayName: 'Thanks Giving',
+      holidayDate: '31st Dec, 2023'
+   }];
+
    const dateFormat = 'YYYY/MM/DD';
 
+
+   const [showExcludedHolidays, setShowExcludedHolidays] = useState(false);
+   const [exceptions, setExceptions] = useState<any>([]);
 
    const getStartAndEndDateTimeDiv = () => {
      return (
@@ -61,6 +83,101 @@ export const Home: React.FC = () => {
         )
    }
 
+   const addNewException = () => {
+      console.log('Exception')
+      exceptions.push({});
+      setExceptions([...exceptions]);
+      console.log(exceptions.length);
+   }
+
+   const onExcludeTechnicalHoliday = (event: any) => {
+      setShowExcludedHolidays(event);
+   }
+
+   const toggleCheckedHolidayList = (event: any, holidayId: number) => {
+      console.log(holidayId, event);
+   }
+
+   // new method
+   const getHolidaySection = () => {
+      console.log('Test');
+      return (
+         <Row>
+         { holidayList.map((holiday: any, index: number) => {
+            return (
+            <Col className={'holidayRow'} key={index} span={12}>
+                <Checkbox defaultChecked onChange={(event) => toggleCheckedHolidayList(event, holiday.id)}>
+                  <span>{holiday.holidayName}</span>  
+                </Checkbox>
+                <span>
+                  {holiday.holidayDate}
+                </span>
+            </Col>
+            )
+
+         })}
+         </Row>
+      );
+       
+   }
+
+   // new method
+   const getExceptionsHTML = () => {
+      return (
+         exceptions.map((exception: any, index: any) => {
+            return (
+               <Row className='exceptionRow' key={index} gutter={[16, 16]}>
+                  <Col span={6}>
+                     <label>
+                        Exception date:
+                     </label>
+                     <DatePicker format={dateFormat} />
+                  </Col>
+                  <Col span={5}>
+                     <label>
+                        Exception date:
+                     </label>
+                     <DatePicker format={dateFormat} />
+                  </Col>
+                  <Col span={5}>
+                    <label>
+                        Exception date:
+                     </label>
+                     <Select
+                     defaultValue={''}
+                     style={{ width: '100%' }}
+                     onChange={() => { }}
+                     placeholder={'Select'}
+                     options={calendarTypes}/>
+                  </Col>
+                  <Col span={6}>
+                    <label>
+                        Exception date:
+                     </label>
+                     <Select
+                     defaultValue={''}
+                     style={{ width: '100%' }}
+                     onChange={() => { }}
+                     placeholder={'Select'}
+                     options={calendarTypes}/>
+                  </Col>
+                  <Col onClick={() => removeException(index)} className='deleteIconDiv' span={2}>
+                   <DeleteOutlined />
+                  </Col>
+                  
+               </Row>
+            );
+         })
+      );
+   }
+
+   //new method
+   const removeException = (index: number) => {
+      console.log(exceptions, index);
+      exceptions.splice(index, 1);
+      setExceptions([...exceptions]);
+   }
+
    const calendarExceptionsSection = () => {
       return (
          <>
@@ -68,16 +185,21 @@ export const Home: React.FC = () => {
                <span className='formTitle'>CALENDAR EXCEPTION</span>
                <span>
                   <span className='superCalendarText'>Exclude technical holiday</span>
-                  <Switch onChange={() => { }} />
+                  <Switch onChange={onExcludeTechnicalHoliday} />
                </span>
             </div>
+            {/**newly added section */}
+            {showExcludedHolidays && getHolidaySection()}
             <Row className='inputContainer'>
                <Col className='add-exception-text' span={24}>
                   <PlusOutlined/>
-                  <span>Add Exception</span>
+                  <span onClick={addNewException}>Add Exception</span>
                </Col>
                
             </Row>
+
+            {/** newly added section */}
+            {exceptions.length > 0 && getExceptionsHTML()}
 
             <Row className='formHeader'>
                <Col span={24}>
